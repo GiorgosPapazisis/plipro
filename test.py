6ea368428c6366a6265243b91531929c1c569eb3
GiorgosPapazisis <123297088+GiorgosPapazisis@users.noreply.github.com>
2025-03-18 15:49:00 +0200

    Update test.py
    Added Activity Entry Widgets and "Add Creation Button"

--- test.py
+++ test.py
@@ -42,27 +42,70 @@ class fileFoundEmpty:
         self.master.title("My Application")
         self.frame = Frame(master)
         self.frame.pack(expand=True, fill="both") 
-        self.frame.pack()
+        self.frame.pack(pady=20)
         self.label = Label(self.frame, text="There are no users or activities")
         self.label.pack()
         self.label = Label(self.frame, text="Lets create a user")
-        self.label.pack()
+        self.label.pack(pady=20)
         self.entry = Entry(self.frame)
-        self.entry.pack()
+        self.entry.pack(pady=20)
         self.button = Button(self.frame, text="Create User", command=lambda:username_entry(self.entry.get(),master))
-        self.button.pack()
+        self.button.pack(pady=20)
         
 class userDashboard:
-    def __init__(self, master,username):
+    def __init__(self, master, username):
+         #-----Title---------
         self.master=master
         self.master.geometry("800x600")
         self.master.title("My Application")
         text="Hi there "+ username
-        self.frame = Frame(master)
-        self.frame.pack(expand=True, fill="both")
-        self.label = Label(self.frame, text=text)
+        self.label = Label(self.master, text=text)
         self.label.pack()
-        print(text)
+
+
+        #-----User Entries---------
+        self.frameEntries = Frame(master)
+        self.labelActivityName=Label(self.frameEntries,text="Name").grid(row=0,column=0,padx=(20,0))
+        self.labelDescription=Label(self.frameEntries,text="Description").grid(row=0,column=1,padx=(20,0))
+        self.labelPriority=Label(self.frameEntries,text="Priority").grid(row=0,column=2,padx=(20,0))
+        self.labelTime=Label(self.frameEntries,text="Time").grid(row=0,column=3,padx=(20,0))
+
+        #--------depracated-------------
+        # self.entryActivityName=Entry(self.frameEntries).grid(row=1,column=0,padx=(20,0))
+        # self.entryDescription=Entry(self.frameEntries).grid(row=1,column=1,padx=(20,0))
+        # self.entryPriority=Entry(self.frameEntries).grid(row=1,column=2,padx=(20,0))
+        # self.entryTime=Entry(self.frameEntries).grid(row=1,column=3,padx=(20,0))
+
+        self.entryActivityName = Entry(self.frameEntries)
+        self.entryActivityName.grid(row=1, column=0, padx=(20, 0))
+
+        self.entryDescription = Entry(self.frameEntries)
+        self.entryDescription.grid(row=1, column=1, padx=(20, 0))
+
+        self.entryPriority = Entry(self.frameEntries)
+        self.entryPriority.grid(row=1, column=2, padx=(20, 0))
+
+        self.entryTime = Entry(self.frameEntries)
+        self.entryTime.grid(row=1, column=3, padx=(20, 0))
+
+        self.frameEntries.pack(pady=40)
+
+        self.buttonAddActivity=Button(self.master, text="Create Activity", command=lambda:addActivity(self.entryActivityName.get(),
+                                                                                                      self.entryDescription.get(),
+                                                                                                      self.entryPriority.get(),
+                                                                                                      self.entryTime.get()
+        ))
+        self.buttonAddActivity.pack()
+
+
+
+        
+
+def addActivity(name, description, priority, time):
+    print(name)
+    print(description)
+    print(priority)
+    print(time)      
 
 
 def username_entry(username, root):
